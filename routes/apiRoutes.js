7c93c32259c5a57173c4455e12933c07938df511
ncmarsh <68823943+ncmarsh@users.noreply.github.com>
2020-11-15 15:23:09 -0800

    added post route to save new notes

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,33 +1,28 @@
 const path = require("path");
 const fs = require("fs");
 const notesdb = require("../db/db.json");
+const { stringify } = require("querystring");
 
 module.exports = function(app) {
     // Displays all notes
-    app.get("/api/notes/", function(req, res) {
+    app.get("/api/notes", function(req, res) {
         return res.json(notesdb);
     });
     
-    // Save new note, add to db and return the new note to the client
-    // take db.json and convert to an array, then push new note into the array, then take new array and put back to db.json
-    // fs, stringify, parse
+    // Saves new note, adds to db and updates the db.json file and the notes list on the browser
     app.post("/api/notes", function(req, res) {
-        console.log(req.body);
-        // res.send(notesdb);
-        console.log(notesdb);
-        let existingNotesArr = JSON.parse(notesdb);
-        console.log(existingNotesArr);
-    
-        fs.readFile("./db/db.json", function(req, res) {
         let newNote = req.body;
         console.log(newNote);
+        console.log(notesdb);
+        notesdb.push(newNote);
+        console.log(notesdb);
     
-    
+        fs.writeFile("./db/db.json", JSON.stringify(notesdb), function(err) {
+            if(err) {
+                return console.log(err);
+            }
+            res.json(newNote);
+            console.log("The file was updated!");
         })
-    
-        // let newNote = req.body;
-        // console.log(newNote);
-        // existingNotesArr.push(newNote);
-        // console.log(existingNotesArr);
     });
 }
